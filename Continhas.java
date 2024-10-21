940450fd982a510f3f39b4b9490812ad033fa702
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-10-21 18:33:06 -0300

    adicionando o classe continha

---
+++ Continhas.java
@@ -0,0 +1,15 @@
+public class Continhas {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+        float d;
+        double e;
+
+        a = 34;
+        b = 4;
+        c = a + b;
+
+        System.out.println("Valor de c" + c);
+    }
+}
